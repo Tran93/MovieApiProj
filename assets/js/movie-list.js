670d52f12b68ad9761eb4a784df882050fe45216
Isiarf <imrfauntleroy@yahoo.com>
2023-11-09 20:56:55 -0800

    Changed the movie-list.js file to be able to see the title, release date, popularity, vote average, and overview of the movies shown.

--- assets/js/movie-list.js
+++ assets/js/movie-list.js
@@ -6,12 +6,42 @@ const options = {
   }
 };
 
-fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(data => {
-    var upcomingMovies = data; // Assign the result to upcomingMovies
-      console.log(upcomingMovies);
-  })
-  .catch(err => console.error(err));
+//overview
+//original_language
+//origin_title
+//title
+//popularity
+//poster_path
+//release_date
+//vote_average
+//vote_count
+
+async function fetchUpcomingMovies() {
+  try {
+    const response = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const upcomingMovies = data;
+
+    console.log(upcomingMovies);
+
+    upcomingMovies.results.forEach(movie => {
+      console.log(`
+        \nTitle | ${movie.title}
+        \nRelease Date | ${movie.release_date} 
+        \nPopularity | ${movie.popularity} 
+        \nVote Average | ${movie.vote_average}
+        \nReview | ${movie.overview}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fetchUpcomingMovies();
 
   
