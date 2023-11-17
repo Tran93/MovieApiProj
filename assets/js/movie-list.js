892e3be66ad9901e39da3356b481184b5e888932
Tim Van Deusen <145621518+timvandeusen@users.noreply.github.com>
2023-11-16 20:20:45 -0800

    added carousel for movies

--- assets/js/movie-list.js
+++ assets/js/movie-list.js
@@ -2,51 +2,75 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZWY0YTUyYzFmYjZlNjllNzc1NzU3NDA3N2RlNDRhYSIsInN1YiI6IjY1NGM1ZWQ2NTMyYWNiNTMzNzFmNWNjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OauQL4aPmEx0tUe8ebpLiW8CV7uaaQ3kdM1O3aMw-5M'
-  }
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZWY0YTUyYzFmYjZlNjllNzc1NzU3NDA3N2RlNDRhYSIsInN1YiI6IjY1NGM1ZWQ2NTMyYWNiNTMzNzFmNWNjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OauQL4aPmEx0tUe8ebpLiW8CV7uaaQ3kdM1O3aMw-5M',
+  },
 };
 
-//-------- Use of the API --------//
+let currentPage = 1; 
+const movieCarousel = document.getElementById('movieCarousel');
 
-//overview
-//original_language
-//origin_title
-//title
-//popularity
-//poster_path
-//release_date
-//vote_average
-//vote_count
 
-// Create a function that can be easily modified to fetch different movies.
-async function fetchUpcomingMovies() {
+fetchAndDisplayMoviesWithDelay(currentPage);
+
+
+setInterval(() => {
+  currentPage++;
+  fetchAndDisplayMoviesWithDelay(currentPage);
+}, 5000);
+
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchAndDisplayMoviesWithDelay(page) {
   try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options);
-    
+    const apiKey = '51DDA431-DE95-4ECF-A96B-BC92A71E8EA1';
+    const apiUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=${page}`;
+    const response = await fetch(apiUrl, options);
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     const data = await response.json();
-    const upcomingMovies = data;
+    const upcomingMovies = data.results;
 
-    console.log(upcomingMovies);
+    
+    console.log('API Response:', data);
 
-    upcomingMovies.results.forEach(movie => {
-      console.log(`
-        \nTitle | ${movie.title}
-        \nRelease Date | ${movie.release_date} 
-        \nPopularity | ${movie.popularity} 
-        \nVote Average | ${movie.vote_average}
-        \nReview | ${movie.overview}`);
+    
+    movieCarousel.innerHTML = '';
+
+    upcomingMovies.forEach(movie => {
+      const movieCard = createMovieCard(movie);
+      movieCarousel.appendChild(movieCard);
     });
-// TODO: Display the next set of movies from the next page.
-
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(`Error fetching upcoming movies: ${error.message}`);
   }
-};
+}
 
-fetchUpcomingMovies();
+function createMovieCard(movie) {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('movie-card');
 
   
+  const title = document.createElement('h3');
+  title.textContent = movie.title;
+
+  
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  
+  console.log('Poster URL:', posterUrl);
+
+  const poster = document.createElement('img');
+  poster.src = posterUrl;
+  poster.alt = movie.title;
+
+  movieCard.appendChild(title);
+  movieCard.appendChild(poster);
+
+  return movieCard;
+}
